414de36458887e16c3cc99cde7b947d8d8ceb4ea
Jaydeep <jaydeep.w@gmail.com>
2014-03-08 21:28:25 +0530

    works

---
+++ restart_adb.py
@@ -0,0 +1,28 @@
+import os
+
+filename = "path.txt"
+
+# copy ADB path to a file
+os.system( "which adb > " + filename )
+
+# form 2-pair of the result
+lines = tuple( open(filename, 'r') )
+
+adb_path = lines[0]
+
+if adb_path is None:
+	print "ADB path not found"
+else:
+	adb_path = adb_path.strip()
+
+print "\nADB path ", adb_path
+
+kill_command = "sudo " + adb_path + " kill-server"
+
+print "===> Kill ADB..."
+os.system(kill_command)
+
+start_command = "sudo " + adb_path + " start-server"
+
+print "===> Start ADB..."
+os.system(start_command)
